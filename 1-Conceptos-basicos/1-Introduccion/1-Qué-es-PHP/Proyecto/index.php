9762c57c5ec3e8d9ab754f97d17242622464ba4b
codefuncode <codefuncode@protonmail.com>
2020-04-30 20:31:44 -0400

    Lectura de documentación y practicas #  vacío aborta el commit.

---
+++ 1-Conceptos-basicos/1-Introduccion/1-Qué-es-PHP/Proyecto/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8"/>
+    <title>
+      Que es PHP
+    </title>
+  </head>
+  <body>
+    <?php echo "¡Hola, soy un script de PHP!" ;?>
+  </body>
+</html>
---
+++ 3-Referencia-del-lenguaje/4-Comentarios/Proiyecto/php/code.php
@@ -0,0 +1,14 @@
+
+<?php
+//  Los textos aquí incorporado fueron tomados de la pagina oficial de php  con  fines didácticos. nada que argumentar en cuando al tema
+echo 'Esto es una prueba';
+
+// Esto es un comentario al estilo de c++ de una sola línea
+
+/* Esto es un comentario multilínea
+y otra línea de comentarios */
+
+echo 'Esto es otra prueba';
+echo 'Una prueba final';
+
+# Esto es un comentario al estilo de consola de una sola línea
